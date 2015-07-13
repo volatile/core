972ead72b2f846602c3f28a120a2926a26be915c
Arthur White <arthur@whitedevops.com>
2015-07-13 23:41:20 +0200

    Add coreutil package with ResponseStatus() helper

---
+++ coreutil/status.go
@@ -0,0 +1,24 @@
+package coreutil
+
+import (
+	"net/http"
+	"reflect"
+)
+
+// ResponseStatus returns the HTTP response status.
+// Be careful that the status is only set by the server after the ResponseWriter.WriteHeader() has been called.
+func ResponseStatus(w http.ResponseWriter) int {
+	return int(httpResponseStruct(reflect.ValueOf(w)).FieldByName("status").Int())
+}
+
+// httpResponseStruct returns the response structure after going trough all theintermediary ResponseWriterBinder.
+func httpResponseStruct(v reflect.Value) reflect.Value {
+	switch v.Type().String() {
+	case "core.ResponseWriterBinder":
+		return httpResponseStruct(v.FieldByName("ResponseWriter").Elem())
+	case "*http.response":
+		return v.Elem()
+	default:
+		panic("core: call of httpResponseStruct on unknown interface type")
+	}
+}
