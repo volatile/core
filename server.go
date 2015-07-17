f3745c8104ded12e47cd49d9100a6b12db1b7c13
Arthur White <arthur@whitedevops.com>
2015-07-17 23:34:31 +0200

    Add BeforeRun feature

--- server.go
+++ server.go
@@ -11,12 +11,18 @@ var (
 	// It can be used by handlers to provide different logic for this environment.
 	Production bool
 	port       int
+	beforeRun  []func()
 )
 
 func init() {
 	flag.BoolVar(&Production, "production", false, "run the server in production environment")
 	flag.IntVar(&port, "port", 8080, "the port to listen on")
 	flag.Parse()
+}
+
+// BeforeRun adds a function that will be triggered before runnning the server.
+func BeforeRun(f func()) {
+	beforeRun = append(beforeRun, f)
 }
 
 // Run starts the server for listening and serving.
@@ -29,6 +35,10 @@ func Run() {
 	Use(func(c *Context) {
 		http.Error(c.ResponseWriter, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	})
+
+	for _, f := range beforeRun {
+		f()
+	}
 
 	panic(http.ListenAndServe(":"+strconv.Itoa(port), handlers))
 }
