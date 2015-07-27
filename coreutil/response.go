de751b7cb623bfe6ca5608bd4bf8479aa24e6128
Arthur White <arthur@whitedevops.com>
2015-07-27 21:44:30 +0200

    Get context instead of response writer

--- coreutil/response.go
+++ coreutil/response.go
@@ -3,12 +3,14 @@ package coreutil
 import (
 	"net/http"
 	"reflect"
+
+	"github.com/volatile/core"
 )
 
 // ResponseStatus returns the HTTP response status.
 // Remember that the status is only set by the server after ResponseWriter.WriteHeader() has been called.
-func ResponseStatus(w http.ResponseWriter) int {
-	return int(httpResponseStruct(reflect.ValueOf(w)).FieldByName("status").Int())
+func ResponseStatus(c *core.Context) int {
+	return int(httpResponseStruct(reflect.ValueOf(c.ResponseWriter)).FieldByName("status").Int())
 }
 
 // httpResponseStruct returns the response structure after going trough all the intermediary responseWriter Binders.
@@ -24,8 +26,8 @@ func httpResponseStruct(v reflect.Value) reflect.Value {
 }
 
 // SetDetectedContentType detects and sets the correct content type.
-func SetDetectedContentType(w http.ResponseWriter, b []byte) {
-	if len(w.Header().Get("Content-Type")) == 0 {
-		w.Header().Set("Content-Type", http.DetectContentType(b))
+func SetDetectedContentType(c *core.Context, b []byte) {
+	if len(c.ResponseWriter.Header().Get("Content-Type")) == 0 {
+		c.ResponseWriter.Header().Set("Content-Type", http.DetectContentType(b))
 	}
 }
