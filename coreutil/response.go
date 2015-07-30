099b6e1070718c705d080ee116b75c6425506aa4
Arthur White <arthur@whitedevops.com>
2015-07-30 13:16:36 +0200

    Fix coreutil

--- coreutil/response.go
+++ coreutil/response.go
@@ -8,28 +8,26 @@ import (
 )
 
 // ResponseStatus returns the HTTP response status.
-// Remember that the status is only set by the server after ResponseWriter.WriteHeader() has been called.
+// Remember that the status is only set by the server after "ResponseWriter.WriteHeader()"" has been called.
 func ResponseStatus(c *core.Context) int {
 	return int(httpResponseStruct(reflect.ValueOf(c.ResponseWriter)).FieldByName("status").Int())
 }
 
-// httpResponseStruct returns the response structure after going trough all the intermediary responseWriter Binders.
+// httpResponseStruct returns the response structure after going trough all the intermediary response writers.
 func httpResponseStruct(v reflect.Value) reflect.Value {
 	switch v.Type().String() {
-	case "core.ResponseWriterBinder":
-		return httpResponseStruct(v.FieldByName("ResponseWriter").Elem())
 	case "*http.response":
 		return v.Elem()
 	default:
-		panic("coreutil: call of httpResponseStruct on unknown interface type")
+		return httpResponseStruct(v.FieldByName("ResponseWriter").Elem())
 	}
 }
 
 // SetDetectedContentType detects and sets and returns the response content type.
 func SetDetectedContentType(c *core.Context, b []byte) string {
 	ct := c.ResponseWriter.Header().Get("Content-Type")
-	if len(ct) == 0 {
-		ct := http.DetectContentType(b)
+	if ct == "" {
+		ct = http.DetectContentType(b)
 		c.ResponseWriter.Header().Set("Content-Type", ct)
 	}
 	return ct
