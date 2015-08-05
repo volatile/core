f767471d697d2326c94b490e767fc32550f45492
Arthur White <arthur@whitedevops.com>
2015-08-05 14:28:44 +0200

    Get package name automatically

--- log/log.go
+++ log/log.go
@@ -3,12 +3,27 @@ package log
 import (
 	"log"
 	"runtime"
+	"strings"
 )
 
 // Stack logs the package pack with an error err and the stack trace.
-func Stack(pack string, err error) {
-	const size = 64 << 10
-	buf := make([]byte, size)
-	buf = buf[:runtime.Stack(buf, false)]
-	log.Printf("%s: %v\n%s", pack, err, buf)
+func Stack(err error) {
+	stack := make([]byte, 64<<10)
+	stack = stack[:runtime.Stack(stack, false)]
+
+	if pack, ok := callerPackage(); ok {
+		log.Printf("%s: %v\n%s", pack, err, stack)
+	} else {
+		log.Printf("%v\n%s", err, stack)
+	}
+}
+
+func callerPackage() (pack string, ok bool) {
+	var pc uintptr
+	if pc, _, _, ok = runtime.Caller(2); !ok {
+		return
+	}
+	path := strings.Split(runtime.FuncForPC(pc).Name(), "/")
+	pack = strings.Split(path[len(path)-1], ".")[0]
+	return
 }
